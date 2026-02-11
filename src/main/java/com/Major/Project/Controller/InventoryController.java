a284aebd0c244c76d7227ed06edd0d4046b930a2
Reshma <rshmprvn@gmail.com>
2026-02-11 12:50:00 -0500

    Implement role-based access control (Admin, Doctor, Patient) for API endpoints #64

--- src/main/java/com/Major/Project/Controller/InventoryController.java
+++ src/main/java/com/Major/Project/Controller/InventoryController.java
@@ -6,6 +6,7 @@ import com.Major.Project.Service.InventoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,31 +17,44 @@ public class InventoryController {
     @Autowired
     private InventoryService inventoryService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public List<InventoryDTO> getItems(){
         return inventoryService.getAllItems();
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{inventoryId}")
     public InventoryDTO getItemsById(@PathVariable Long inventoryId){
         return inventoryService.getItemsById(inventoryId);
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/status/{status}")
     public List<InventoryDTO> getItemsByStatus(@PathVariable String status){
         return inventoryService.getItemByStatus(status);
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/category/{category}")
     public List<InventoryDTO> getItemsByCategory(@PathVariable String category){
         return inventoryService.getItemByCategory(category);
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public InventoryDTO createItems(@RequestBody Inventory inventory){
         return inventoryService.createItem(inventory);
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{inventoryId}")
     public ResponseEntity<Void> deleteItem(@PathVariable Long inventoryId){
         inventoryService.deleteItem(inventoryId);
         return ResponseEntity.noContent().build();
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{inventoryId}")
     public ResponseEntity<InventoryDTO> updateItems(@PathVariable Long inventoryId,@RequestBody Inventory inventory){
        InventoryDTO updated= inventoryService.updateItem(inventoryId,inventory);
